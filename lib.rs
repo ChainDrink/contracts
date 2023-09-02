63871037d9a10256ccc152b51c4ecf1b5a40218f
Piotr Szacilowski <szacilowski.piotr@gmail.com>
2023-09-02 14:20:56 +0200

    add balance storage and unit tests

--- lib.rs
+++ lib.rs
@@ -2,9 +2,12 @@
 
 #[ink::contract]
 mod chaindrink {
+    use ink::storage::Mapping;
+
     #[ink(storage)]
     pub struct ChainDrink {
         owner: AccountId,
+        balances: Mapping<AccountId, Balance>,
     }
 
     #[ink(event)]
@@ -18,7 +21,14 @@ mod chaindrink {
     impl ChainDrink {
         #[ink(constructor)]
         pub fn new(init_owner: AccountId) -> Self {
-            Self { owner: init_owner }
+            let balances = Mapping::default();
+            Self { owner: init_owner, balances }
+        }
+
+        #[ink(message)]
+        pub fn get_balance(&self) -> Option<Balance> {
+            let caller = self.env().caller();
+            self.balances.get(caller)
         }
 
         #[ink(message)]
@@ -29,8 +39,14 @@ mod chaindrink {
         #[ink(message, payable)]
         pub fn receive(&mut self) {
             assert!(self.env().transferred_value() > 0);
+            let caller = self.env().caller();
+
+            let balance = self.balances.get(caller).unwrap_or(0);
+            let endowment = self.env().transferred_value();
+            self.balances.insert(caller, &(balance + endowment));
+
             self.env().emit_event(Received {
-                from: Some(self.env().caller()),
+                from: Some(caller),
                 value: self.env().transferred_value(),
             });
         }
@@ -43,6 +59,40 @@ mod chaindrink {
             
             let result = self.env().transfer(self.owner, amount);
             assert_eq!(result, Ok(()), "Transfer failed");
+        }
+
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        fn default_accounts() -> ink_env::test::DefaultAccounts<ink_env::DefaultEnvironment> {
+            ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+        }
+    
+        #[ink::test]
+        fn constructor_works() {
+            let accounts = default_accounts();
+            let chaindrink = ChainDrink::new(accounts.alice);
+            assert_eq!(chaindrink.owner(), accounts.alice);
+        }
+
+        #[ink::test]
+        fn receive_works() {
+            let accounts = default_accounts();
+            let mut chaindrink = ChainDrink::new(accounts.alice);
+            
+            let initial_balance = chaindrink.get_balance().unwrap_or(0);
+            assert_eq!(initial_balance, 0);
+
+            let endowment = 50;
+            ink::env::test::set_caller::<ink::env::DefaultEnvironment>(accounts.bob);
+            ink::env::test::set_value_transferred::<ink::env::DefaultEnvironment>(endowment);
+            chaindrink.receive();
+    
+            let new_balance = chaindrink.get_balance().unwrap_or(0);
+            assert_eq!(new_balance, 50);
         }
 
     }
